27ad14939a55a5bdb3fb93e91d61538d29007619
geranhuns <gera.nuncio@gmail.com>
2024-01-14 20:13:23 -0600

    started filters

--- js/main.js
+++ js/main.js
@@ -66,6 +66,7 @@ let getAllUsers = async () => {
     
     let image = document.createElement("img");
     image.classList.add("card-img-top");
+    //image.style.display="none"
     image.src = `${postImage}`;
     
     let userInfo = await printOnePostUserInfo(username,date)
@@ -78,6 +79,58 @@ let getAllUsers = async () => {
     postTitle.append(postTitleText)
 
     divPost.append(postTitle)
+    
+    let restoDelPostDiv = document.createElement("div")
+    restoDelPostDiv.classList.add("Interactions")
+     restoDelPostDiv.innerHTML=`                        
+                            <div class="emoticones-comentarios">
+
+                                <span class="reacciones">
+                                    <span><img
+                                            src="https://dev.to/assets/fire-f60e7a582391810302117f987b22a8ef04a2fe0df7e3258a5f49332df1cec71e.svg"
+                                            width="18" height="18"></span>
+                                    <span><img
+                                            src="https://dev.to/assets/raised-hands-74b2099fd66a39f2d7eed9305ee0f4553df0eb7b4f11b01b6b1b499973048fe5.svg"
+                                            width="18" height="18"></span>
+                                    <span><img
+                                            src="https://dev.to/assets/exploding-head-daceb38d627e6ae9b730f36a1e390fca556a4289d5a41abb2c35068ad3e2c4b5.svg"
+                                            width="18" height="18"></span>
+                                    <span><img
+                                            src="https://dev.to/assets/multi-unicorn-b44d6f8c23cdd00964192bedc38af3e82463978aa611b4365bd33a0f1f4f3e97.svg"
+                                            width="18" height="18"></span>
+                                    <span><img
+                                            src="https://dev.to/assets/sparkle-heart-5f9bee3767e18deb1bb725290cb151c25234768a0e9a2bd39370c382d02920cf.svg"
+                                            width="18" height="18"></span>
+                                </span>
+                                <span class="contadorReacciones">
+                                    340 reactions
+                                </span>
+
+                                <span class="contadorcomentarios">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" role="img"
+                                        aria-labelledby="aewrn1ygzu2zckt6iwmhazo9x6f8e3kt" class="crayons-icon">
+                                        <title id="aewrn1ygzu2zckt6iwmhazo9x6f8e3kt">Comments</title>
+                                        <path
+                                            d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z">
+                                        </path>
+                                    </svg>
+                                    104 Coments
+                                </span>
+
+                            </div>
+                            <div class="visitas">
+                                4 min read
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true">
+                                    <path
+                                        d="M6.75 4.5h10.5a.75.75 0 01.75.75v14.357a.375.375 0 01-.575.318L12 16.523l-5.426 3.401A.375.375 0 016 19.607V5.25a.75.75 0 01.75-.75zM16.5 6h-9v11.574l4.5-2.82 4.5 2.82V6z">
+                                    </path>
+                                </svg></span>
+                            </div>
+                        </div>
+                    </div>
+                `
+    divPost.append(restoDelPostDiv)
+    
     postsArea.append(divPost)
     
   }
@@ -143,101 +196,29 @@ let getAllUsers = async () => {
       printOnePostPart1(post)
 
     })
-
-
-  }
-
-
-  //Esta función replica tantas veces sea necesario la función createPostItem
-  
-  
-  
-  
-  /*
+    
+}
  
 
-const createPostItem = (post) => {
-    let { postImage, username, title, date, hashtags, key } = post;
-    // let userIndex = allUsersArray.findIndex(
-    //   (user) => user.username === username
-    // );
-    // let { profilePicture, bio, joined } = users[userIndex];
 
-    // Crear elementos
-    
-    let divPost = document.createElement("div");
-    divPost.classList.add("col-12", "card", "postSpacing");
+const asideListFilter = async (hashtagEspecifico)=> {
+  
+  let allPostsObject = await getAllPosts()
+  console.log(allPostsObject)
+  let allPostsArray = Object.keys(allPostsObject).map((key)=>({...allPostsObject[key],key }))
+  console.log(allPostsArray)
+  
+  let  postsWithHashtags = allPostsArray.filter((post)=>
+     post.hashtags.hashtagEspecifico
+     )
+     console.log(postsWithHashtags);
+  console.log(postsWithHashtags)
+  let asideRigth = document.getElementById("asideRigth")
 
-    let PostImage = document.createElement("img");
-    PostImage.classList.add("card-img-top");
-    PostImage.src = `${postImage}`;
-    let CardBody = document.createElement("div");
-    CardBody.classList.add("card-body");
+}
 
-    let UserInfo = document.createElement("div");
-    UserInfo.classList.add("foto", "row");
-
-    let UserProfilePicture = document.createElement("div");
-    UserProfilePicture.classList.add("col.2");
-    UserProfilePicture.style.width = "4.5rem";
-    //UserProfilePicture.src = profilePicture;
-    let userJoinedDiv = document.createElement("div");
-    userJoinedDiv.classList.add("col-6");
-
-    let Username = document.createElement("h5");
-    let UsernameText = document.createTextNode(`${username}`);
-    let postDate = document.createElement("h6");
-    postDate.classList.add("fw-light");
-    let postDateText = document.createTextNode(`${date}`);
+printAllPosts()
+  
 
 
-    let postTitle = document.createElement("h2")  
-    postTitle.classList.add("fs-1","postTitle","px-5")
-    let postTitleText = document.createTextNode(`${title}`)
-    postTitle.append(postTitleText)
-    
-    
-     let HashtagsDiv = document.createElement("div")
-     HashtagsDiv.classList.add("Hastag")
-     let A1 = document.createElement("a")
-     A1.classList.add("a1")
-     let A1Text = document.createTextNode(`${hashtags[1]}`)
-     A1.append(A1Text)
-     let A2 = document.createElement("a")
-     A2.classList.add("a2")
-     let A2Text = document.createTextNode(`${hashtags[2]}`)
-     A2.append(A2Text)
-     let A3 = document.createElement("a")
-     A3.classList.add("a3")
-     let A3Text = document.createTextNode(`${hashtags[3]}`)
-     A3.append(A3Text)
-     let A4 = document.createElement("a")
-     A4.classList.add("a4")
-     let A4Text = document.createTextNode(`${hashtags[4]}`)
-     A4.append(A4Text)
-     HashtagsDiv.append(A1,A2,A3,A4) 
-
-    CardBody.append()
-    // Construir la estructura del DOM
-    postDate.append(postDateText);
-    Username.append(UsernameText);
-    userJoinedDiv.append(Username, postDate);
-    UserInfo.append(UserProfilePicture, userJoinedDiv);
-    CardBody.append(UserInfo);
-    divPost.append(PostImage, CardBody);
-  }
-//let postAllPosts = getAllPosts()
-const postallPosts = (posts) => {
-
-let postsArea = document.getElementById("postsArea");
-  postsArea.innerHTML = "";
-  let postsArray = Object.keys(posts).map((key) => ({ ...posts[key], key }));
-
-  postsArray.forEach((post) => {
-    let postItem = createPostItem(post);
-    postsArea.append(postItem);
-
-    
-  });
-}; 
-*/
+  
