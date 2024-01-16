97dd1cdb37bfc2618c1b5622f9c3e2b0b03c9df1
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-15 21:59:59 -0600

    login page

--- js/login.js
+++ js/login.js
@@ -1,3 +1,36 @@
+document
+  .getElementById("formulario")
+  .addEventListener("click", function (event) {
+    let email = document.getElementById("emailInput").value;
+
+    if (email.trim() === "") {
+      document.getElementById("emailFieldError").innerHTML = "Ingresa un email";
+      event.preventDefault();
+      return;
+    } else {
+      document.getElementById("emailFieldError").innerHTML = "";
+    }
+
+    let password = document.getElementById("passInput").value;
+
+    if (password.trim() === "") {
+      document.getElementById("passFieldError").innerHTML =
+        "Ingresa una contraseña";
+
+      event.preventDefault();
+      return;
+    } else {
+      document.getElementById("passFieldError").innerHTML = "";
+    }
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkNhcmxvcyBIZXJtb3NpbGxvIiwiaWF0IjoxNTE2MjM5MDIyfQ.Dwj6NgrYTWfePd_9Y_4a77zrxY-u78g3PY_but6Ng8c";
+
+    localStorage.setItem("token", token);
+
+    window.open("index.html", "_self");
+  });
+
+/*
 let emailValidation = document
   .getElementById("formulario")
   .addEventListener("submit", function (event) {
@@ -30,7 +63,7 @@ let passValidation = document
 if (email != "" && password != "") {
   document
     .getElementById("logInBtn")
-    .addEventListener("sumbit", function (event) {
+    .addEventListener("submit", function (event) {
       const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkNhcmxvcyBIZXJtb3NpbGxvIiwiaWF0IjoxNTE2MjM5MDIyfQ.Dwj6NgrYTWfePd_9Y_4a77zrxY-u78g3PY_but6Ng8c";
 
@@ -39,13 +72,4 @@ if (email != "" && password != "") {
       window.open("index.html", "_self");
     });
 }
-
-/*const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkNhcmxvcyBIZXJtb3NpbGxvIiwiaWF0IjoxNTE2MjM5MDIyfQ.Dwj6NgrYTWfePd_9Y_4a77zrxY-u78g3PY_but6Ng8c";
-
-let userToken = localStorage.setItem("token", token);
-
-console.log(userToken);
-
-document.getElementById("loginBtn").addEventListener("click", () => {});
 */
