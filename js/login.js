21c990d5a38315079a38e5e1f81aace4717ac587
gdHermosillo <gadiel.ddelgado@gmail.com>
2024-01-15 21:24:47 -0600

    form validation

---
+++ js/login.js
@@ -0,0 +1,51 @@
+let emailValidation = document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    let email = document.getElementById("emailInput").value;
+
+    if (email.trim() == "") {
+      document.getElementById("emailFieldError").innerHTML = "Ingresa un email";
+
+      event.preventDefault();
+    } else {
+      document.getElementById("emailFieldError").innerHTML = "";
+    }
+  });
+
+let passValidation = document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    let password = document.getElementById("passInput").value;
+
+    if (password.trim() === "") {
+      document.getElementById("passFieldError").innerHTML =
+        "Ingresa una contraseña";
+
+      event.preventDefault();
+    } else {
+      document.getElementById("passFieldError").innerHTML = "";
+    }
+  });
+
+if (email != "" && password != "") {
+  document
+    .getElementById("logInBtn")
+    .addEventListener("sumbit", function (event) {
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkNhcmxvcyBIZXJtb3NpbGxvIiwiaWF0IjoxNTE2MjM5MDIyfQ.Dwj6NgrYTWfePd_9Y_4a77zrxY-u78g3PY_but6Ng8c";
+
+      localStorage.setItem("token", token);
+
+      window.open("index.html", "_self");
+    });
+}
+
+/*const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkNhcmxvcyBIZXJtb3NpbGxvIiwiaWF0IjoxNTE2MjM5MDIyfQ.Dwj6NgrYTWfePd_9Y_4a77zrxY-u78g3PY_but6Ng8c";
+
+let userToken = localStorage.setItem("token", token);
+
+console.log(userToken);
+
+document.getElementById("loginBtn").addEventListener("click", () => {});
+*/
